9f985320c40ddfdcba20eb33bf6c7696d48492c7
ncheng1 <chengmnathan@live.com>
2016-06-06 15:59:36 -0400

    Add files via upload

--- src/main/java/dto/Pharmacy.java
+++ src/main/java/dto/Pharmacy.java
@@ -1,5 +1,6 @@
 package dto;
 
+
 public class Pharmacy {
 	
 	public Pharmacy() {
@@ -27,7 +28,38 @@ public class Pharmacy {
 	public boolean isH24Store() {	return h24Store;	}
 	public void setH24Store(boolean h24Store) {	this.h24Store = h24Store;	}
 	
+	private String address = new String();
+	private Address newAddress = new Address();
+	public void setAddress(String address1, String address2, String city, String state, String zip)
+	{
+		newAddress.setAddress1(address1);
+		newAddress.setAddress2(address2);
+		newAddress.setCity(city);
+		newAddress.setCode(state);
+		newAddress.setZip(zip);
+	}
+	public String getAddressStr()
+	{
+		return this.address;
+	}
+	public Address getAddress()
+	{
+		return this.newAddress;
+	}
+	public void parse()
+	{
+		int firstLine = this.getAddressStr().indexOf("\n");
+		int secondLine = this.getAddressStr().indexOf("\n", firstLine+1);
+		int thirdLine = this.getAddressStr().indexOf(",", secondLine+1);
+		int fourthLine = this.getAddressStr().indexOf("\n", thirdLine+1);
+		this.setAddress(this.getAddressStr().substring(0, firstLine), //address1
+				this.getAddressStr().substring(firstLine+1, secondLine), //address2
+						this.getAddressStr().substring(secondLine+1, thirdLine), //City
+						this.getAddressStr().substring(this.getAddressStr().indexOf(",")+2, fourthLine), //state
+						this.getAddressStr().substring(fourthLine+1, this.getAddressStr().length())); //zip
+	}
 	
+	/*
 	private String address1;	
 	public String getAddress1() {	return address1;	}
 	public void setAddress1(String address1) {	this.address1 = address1;	}
@@ -40,6 +72,7 @@ public class Pharmacy {
 	public String getCity() {	return this.city;	}
 	public void setCity(String city) {	this.city = city;	}
 	
+	
 	private String state;
 	public String getState() {	return this.state;	}
 	public void setState(String state) {	this.state = state;	}
@@ -47,6 +80,7 @@ public class Pharmacy {
 	private String zip;
 	public String getZip() {	return this.zip;	}
 	public void setZip(String zip) {	this.zip = zip;	}
+	*/
 	
 	private String email;
 	public String getEmail() {	return this.email;	}
--- src/main/java/dto/UsState.java
+++ src/main/java/dto/UsState.java
@@ -9,7 +9,23 @@ public class UsState {
 	public UsState() {
 		super();
 	}
+	/*
+	private String address1;	
+	public String getAddress1() {	return address1;	}
+	public void setAddress1(String address) {	this.address1 = address;	}
 	
+	private String address2;
+	public String getAddress2() {	return address2;	}
+	public void setAddress2(String address2) {	this.address2 = address2;	}
+
+	private String city;
+	public String getCity() {	return this.city;	}
+	public void setCity(String city) {	this.city = city;	}
+	
+	private String zip;
+	public String getZip() {	return this.zip;	}
+	public void setZip(String zip) {	this.zip = zip;	}
+	*/
 	private String name;
 	public String getName() {	return name;	}
 	public void setName(String name) {	this.name = name;	}	
@@ -17,5 +33,7 @@ public class UsState {
 	private String code;
 	public String getCode() {	return code;	}
 	public void setCode(String code) {	this.code = code;	}
+	
+
 
 }
