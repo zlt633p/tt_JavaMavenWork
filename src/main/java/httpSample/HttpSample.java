a41069e78662f92ded44ec51066ba44a19963b67
ncheng1 <chengmnathan@live.com>
2016-06-30 10:04:16 -0400

    Add files via upload

--- src/main/java/httpSample/HttpSample.java
+++ src/main/java/httpSample/HttpSample.java
@@ -39,16 +39,21 @@ public class HttpSample {
 	public void init(){
 		httpClient = HttpClients.createDefault();		
 	}
+	
 	public UploadResponse runUploadGet(String fileName)
 	{
 		UploadResponse upload = null;
 		String page = new String("https://drfirst.brickftp.com/api/rest/v1/files/");
 		page = page.concat(fileName);
+		
+		//Authorization
 		String authString = new String("79094c529c0bf4bd2279018800ef40064ab38934:x");
 		byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
 		String authStringEnc = new String(authEncBytes);
 		HttpGet httpGet = new HttpGet(page);
 		httpGet.setHeader("Authorization", "Basic " + authStringEnc);
+		
+		
 		System.out.println(httpGet.getRequestLine());
 		CloseableHttpResponse response = null;
 		try {
@@ -76,6 +81,13 @@ public class HttpSample {
 		}
 		return upload;
 	}
+	
+	/* First step of the upload
+	 * Post to brickftp url files/filename
+	 * stores data from response into UploadResponse
+	 * aws upload link will be used in next step
+	 * ref will be used in closing step
+	 */
 	public UploadResponse runUploadStart(String fileName)
 	{
 		UploadResponse upload = null;
@@ -91,7 +103,7 @@ public class HttpSample {
 			HttpPost httppost = new HttpPost(page);
 		
 			httppost.setHeader("Authorization", "Basic " + authStringEnc);
-			StringEntity input = new StringEntity("{\"action\":\"put\"}");
+			StringEntity input = new StringEntity("{\"action\":\"put\"}"); //request body
 			input.setContentType("application/json");
 			httppost.setEntity(input);
 			System.out.println(httppost.getRequestLine());
@@ -103,7 +115,7 @@ public class HttpSample {
 			System.out.println("Output from Server .... \n");
 			output = br.readLine();
 				System.out.println(output);
-				upload = Json2Codec.unmarshal(UploadResponse.class, output);
+				upload = Json2Codec.unmarshal(UploadResponse.class, output); //unmarshal json payload from response
 				String uploadEscape = new String(StringEscapeUtils.unescapeHtml4(upload.getUpload_uri()));
 				upload.setUpload_uri(uploadEscape);
 			response.close();
@@ -118,6 +130,9 @@ public class HttpSample {
 		return upload;
 	}
 	
+	/*Executes a HttpPut command to the upload link given by the first step (String url)
+	 * returns a null response
+	 */
 	public void runUploadAWSStep(String url, String filePath)
 	{	
 		HttpPut httpPut = new HttpPut(url);
@@ -145,6 +160,10 @@ public class HttpSample {
 		}
 	}
 		
+	/*
+	 * closes the upload after it has completed
+	 * body contains action : end and ref from starting step 
+	 */
 	public void closeUpload(String refId, String fileName)
 	{
 		String page = new String("https://drfirst.brickftp.com/api/rest/v1/files/");
@@ -156,7 +175,7 @@ public class HttpSample {
 		{
 			HttpPost httppost = new HttpPost(page);
 			httppost.setHeader("Authorization", "Basic " + authStringEnc);
-			StringEntity input = new StringEntity("{\"action\":\"end\", \"ref\":\""+ refId+ "\"}");
+			StringEntity input = new StringEntity("{\"action\":\"end\", \"ref\":\""+ refId+ "\"}"); //request body with refId
 			input.setContentType("application/json");
 			httppost.setEntity(input);
 			System.out.println(httppost.getRequestLine());
@@ -179,6 +198,9 @@ public class HttpSample {
 		}
 	}
 	
+	/*downloads file from aws download link, which can be accessed by command runUploadGet or from UploadResponse class
+	 * 
+	 */
 	public void downloadFile(String downloadURL, String filePath)
 	{
 		long startTime = System.currentTimeMillis();
@@ -217,20 +239,23 @@ public class HttpSample {
 		UploadResponse upload = null;
 		UploadResponse download = null;
 		HttpSample sample = new HttpSample();
-		String fileName = new String("testing.zip");
-		String newPath = new String ("C:\\Users\\ncheng\\Downloads\\eclipse-java-kepler-SR2-win32-x86_64.zip");
-		String path = new String("C:\\users\\ncheng\\documents\\testingJava.zip");
+		
+//		String fileName = new String("testing.zip");
+//		String upPath = new String ("C:\\Users\\ncheng\\Downloads\\spring-tool-suite-3.7.3.RELEASE-e4.5.2-win32-x86_64.zip");
+//		String path = new String("C:\\users\\ncheng\\documents\\testingJava.zip");
+		
+		String fileName = new String("testing.txt");
+		String upPath = new String ("testing.txt");
+		String path = new String("C:\\users\\ncheng\\documents\\testingJava.txt");
+		
 		upload = sample.runUploadStart(fileName);
 		
-		sample.runUploadAWSStep(upload.getUpload_uri(), newPath);
+		sample.runUploadAWSStep(upload.getUpload_uri(), upPath);
 		sample.closeUpload(upload.getRef(), fileName);
 		download = sample.runUploadGet(fileName);
 		System.out.println("Paste this link into browser: \n" + download.getDownload_uri());
 		
-		sample.downloadFile(download.getDownload_uri(), path);
-
-	
-		
+		sample.downloadFile(download.getDownload_uri(), path);	
 	}
 
 }
